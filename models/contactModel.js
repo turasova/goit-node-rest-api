fca7dc04ef381fa48eb3f4d91402d1c71fff015f
turasova <134943646+turasova@users.noreply.github.com>
2024-02-23 12:26:35 +0200

    finish HW03

---
+++ models/contactModel.js
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose";
+
+const contactModelSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = model("Contact", contactModelSchema);
+export default Contact;
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,26 +1,39 @@
 import express from "express";
-import {
-  getAllContacts,
-  getOneContact,
-  deleteContact,
-  createContact,
-  updateContact,
-} from "../controllers/contactsControllers.js";
+import controllers from "../controllers/contactsControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import {
   createContactSchema,
   updateContactSchema,
 } from "../schemas/contactsSchemas.js";
+
 const contactsRouter = express.Router();
+const jsonParse = express.json();
 
-contactsRouter.get("/", getAllContacts);
+contactsRouter.get("/", controllers.getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", controllers.getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", controllers.deleteContact);
 
-contactsRouter.post("/", validateBody(createContactSchema), createContact);
+contactsRouter.post(
+  "/",
+  jsonParse,
+  validateBody(createContactSchema),
+  controllers.createContact
+);
 
-contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+contactsRouter.put(
+  "/:id",
+  jsonParse,
+  validateBody(updateContactSchema),
+  controllers.updateContact
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  jsonParse,
+  validateBody(updateContactSchema),
+  controllers.updateStatusContact
+);
 
 export default contactsRouter;

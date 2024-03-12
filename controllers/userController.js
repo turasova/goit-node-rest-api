fc8839d8f11d96cebc43575ee12b17fe25d5e5ab
turasova <134943646+turasova@users.noreply.github.com>
2024-03-12 14:09:58 +0200

    fix error

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,32 +8,38 @@ import * as crypto from "node:crypto";
 const avatarsDir = path.join(process.cwd(), "public/avatars");
 
 async function uploadAvatar(req, res, next) {
-  const { _id } = req.user;
+  try {
+    if (!req.file) {
+      throw HttpError(400, "Avatar must be provided");
+    }
+    const { _id } = req.user;
+    const { path: tmpUpload, originalname } = req.file;
 
-  if (!req.user) {
-    throw HttpError(401, { message: "Not authorized" });
-  }
-  const { path: tmpUpload, originalname } = req.file;
+    if (!req.user) {
+      throw HttpError(401, { message: "Not authorized" });
+    }
+    await Jimp.read(tmpUpload)
+      .then((avatar) => {
+        return avatar.resize(250, 250).quality(60).write(tmpUpload);
+      })
+      .catch((error) => {
+        throw error;
+      });
 
-  await Jimp.read(tmpUpload)
-    .then((avatar) => {
-      return avatar.resize(250, 250).quality(60).write(tmpUpload);
-    })
-    .catch((error) => {
-      throw error;
+    const prefix = crypto.randomUUID();
+    const fileName = `${prefix}_${_id}_${originalname}`;
+    const publicUpload = path.join(avatarsDir, fileName);
+
+    await fs.rename(tmpUpload, publicUpload);
+    const avatarURL = path.join("avatars", fileName);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({
+      avatarURL,
     });
-
-  const prefix = crypto.randomUUID();
-  const fileName = `${prefix}_${_id}_${originalname}`;
-  const publicUpload = path.join(avatarsDir, fileName);
-
-  await fs.rename(tmpUpload, publicUpload);
-  const avatarURL = path.join("avatars", fileName);
-  await User.findByIdAndUpdate(_id, { avatarURL });
-
-  res.json({
-    avatarURL,
-  });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export default uploadAvatar;

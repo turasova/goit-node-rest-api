75f4602ea0d173dd632ee14f32e87a5d65e111a1
turasova <134943646+turasova@users.noreply.github.com>
2024-03-13 21:38:31 +0200

    HW06 mail verify

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,22 @@
+import "dotenv/config";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 import HttpError from "../helpers/HttpError.js";
 import gravatar from "gravatar";
+import nodemailer from "nodemailer";
+import crypto from "node:crypto";
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+});
+
+const verificationToken = crypto.randomUUID();
 
 async function register(req, res, next) {
   const { email, password } = req.body;
@@ -16,13 +30,21 @@ async function register(req, res, next) {
     }
 
     const passwordHash = await bcrypt.hash(password, 10);
-
     const avatarURL = gravatar.url(email, { d: "identicon" });
+
+    await transport.sendMail({
+      to: email,
+      from: "turasova1986@gmail.com",
+      subject: "Welcome to PhoneBook",
+      html: `To confirm you registration please click on the <a href="http://localhost:3000/users/verify/${verificationToken}">Link</a>`,
+      text: `To confirm you registration please open the link http://localhost:3000/users/verify/${verificationToken}`,
+    });
 
     const newUser = await User.create({
       ...req.body,
       password: passwordHash,
       avatarURL,
+      verificationToken,
     });
 
     res.status(201).json({
@@ -53,6 +75,10 @@ async function login(req, res, next) {
     if (!isMatch) {
       console.log("Password");
       throw HttpError(401, "Email or password is incorrect");
+    }
+
+    if (user.verify === false) {
+      throw HttpError(401, "Your account is not verified");
     }
 
     const token = jwt.sign(
@@ -98,4 +124,53 @@ async function current(req, res) {
   });
 }
 
-export default { register, login, logout, current };
+async function verify(req, res, next) {
+  const { verificationToken } = req.params;
+
+  try {
+    const user = await User.findOne({ verificationToken });
+
+    if (!user) {
+      throw HttpError(404, "User not found");
+    }
+
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: null,
+    });
+
+    res.send({ message: "Verification successful" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function resendVerifyEmail(req, res, next) {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw HttpError(400, "Missing required field email");
+    }
+
+    if (user.verify) {
+      throw HttpError(400, "Verification has already been passed");
+    }
+
+    await transport.sendMail({
+      to: email,
+      from: "turasova1986@gmail.com",
+      subject: "Welcome to PhoneBook",
+      html: `To confirm you registration please click on the <a href="http://localhost:3000/users/verify/${verificationToken}">Link</a>`,
+      text: `To confirm you registration please open the link http://localhost:3000/users/verify/${verificationToken}`,
+    });
+
+    res.send({ message: "Verification email sent" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export default { register, login, logout, current, verify, resendVerifyEmail };
--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -12,3 +12,9 @@ export const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+export const userEmailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
